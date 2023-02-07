b8c57fea5fe2b711124192728ada997c851a7137
huynhtuan17ti <huynhminhtuan6429@gmail.com>
2023-02-07 23:39:24 +0700

    add emplace_back

---
+++ std_optim/opt_007/README.md
@@ -0,0 +1,17 @@
+## `emplace_back` in vector (since C++11)
+
+### Short introduce
+Inserts a new element at the end of the vector, right after its current last element. This new element is constructed in place using args as the arguments for its constructor.
+
+### What is the difference between `push_back` and `emplace_back`?
+- While performing `push_back`, the object is copied and moved into the container.
+- Same as `push_back`, `emplace_back` is still inserting a object but the variadic arguments will be forwarded into the constructor of appended item.
+
+### Run with bazel
+```bash
+bazel run //std_optim/opt_007:exec
+```
+
+### References
+- [cppreference](https://en.cppreference.com/w/cpp/container/vector/emplace_back)
+- [stackoverflow](https://stackoverflow.com/questions/4303513/push-back-vs-emplace-back)
---
+++ std_optim/opt_007/code.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <utility>
+#include <vector>
+#include <chrono>
+#include <functional>
+using namespace std;
+
+int main() {
+	constexpr int N = 1e6;
+	using KeyValue = pair<string, int>;
+
+    auto benchmark_push_back = []() {
+		vector<KeyValue> test;
+        const auto start = std::chrono::high_resolution_clock::now();
+        
+        for(int x = 1; x <= N; ++x)
+            test.push_back(KeyValue(to_string(x), x));
+
+        const std::chrono::duration<long double> diff =
+            std::chrono::high_resolution_clock::now() - start;
+        
+        cout << "Time: " << int(diff.count() * 1e9) << " nanosecs (push_back)" << '\n'; 
+    };
+
+    auto benchmark_emplace_back = []() {
+		vector<KeyValue> test;
+        const auto start = std::chrono::high_resolution_clock::now();
+        
+        for(int x = 1; x <= N; ++x)
+            test.emplace_back(KeyValue(to_string(x), x));
+
+        const std::chrono::duration<long double> diff =
+            std::chrono::high_resolution_clock::now() - start;
+        
+        cout << "Time: " << int(diff.count() * 1e9) << " nanosecs (emplace_back)" << '\n'; 
+    };
+
+	for(int cnt = 1; cnt <= 5; ++cnt) {
+		cout << "Run #" << cnt << ":\n";
+		benchmark_push_back();
+		benchmark_emplace_back();
+	}
+}
